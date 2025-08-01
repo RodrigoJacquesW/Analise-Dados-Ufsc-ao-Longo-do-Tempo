f1abe3200a1a615f1af17bf6e560de793574f5aa
Rodrigo Jacques Wolff <100800770+RodrigoJacquesW@users.noreply.github.com>
2025-07-31 23:06:52 -0300

    Update tratament_nc_pdf.py

--- scripts/tratament_nc_pdf.py
+++ scripts/tratament_nc_pdf.py
@@ -24,18 +24,20 @@ nc_2013_2019.to_csv("data/bronze/nc_2013_2019.csv", index=False)
 
 dados1 = []
 
-for ano1 in range(2020,2026):
+for ano1 in range(2020, 2026):
     with pdfplumber.open(f'data/nota_de_corte/nota_de_corte_{ano1}.pdf') as pdf1:
-        for pages1 in pdf.pages: 
-            tabela1 = pages1.extract_table()
+        for page in pdf1.pages:  
+            tabela1 = page.extract_table() 
             if tabela1:
-                df1 = pd.DataFrame(tabela [1:], columns = tabela[0])
-                df1 = df1.drop([0],axis = 0)
+                df1 = pd.DataFrame(tabela1[1:], columns=tabela1[0])
                 df1 = df1.iloc[:, :5]
                 df1.columns = ['Codigo', 'Nome do Curso', 'Acertos do Primeiro', 'Acertos do Ultimo', 'Total de Vagas']
                 df1['Ano'] = ano1
+                df1 = df1[df1['Codigo'].notna() & (df1['Codigo'] != '')]
                 dados1.append(df1)
+
 nc_2020_2026 = pd.concat(dados1, ignore_index=True)
-nc_2020_2026.to_csv('data/bronze/nc_2020_2026.csv',index = False)
+nc_2020_2026.to_csv('data/bronze/nc_2020_2026.csv', index=False)
+print(nc_2020_2026)
 
 
